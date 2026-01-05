6e3a1f79b911791804f0a1ad455b55778a206ceb
ivanbap <ivanbodemberg@gmail.com>
2026-01-04 21:27:10 -0300

    att header btn mobile

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+$(document).ready(function() {
+    $('.btn-mobile').on('click', function () {
+        $('.mobile-menu').toggleClass('active');
+        $('.btn-mobile').find('i').toggleClass('fa-x');
+});
+});
+
+//btn faq
 const faqItems = document.querySelectorAll('.faq-item');
 
 faqItems.forEach(item => {
   const question = item.querySelector('.faq-question');
   
   question.addEventListener('click', () => {
-    // Fecha outros itens abertos (opcional)
-    faqItems.forEach(otherItem => {
-      if (otherItem !== item) otherItem.classList.remove('active');
-    });
-
     // Abre ou fecha o item clicado
     item.classList.toggle('active');
   });
