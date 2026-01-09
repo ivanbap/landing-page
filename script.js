a69780d09eb8e7ad5f49ac9f868073cc68bd9122
ivanbap <ivanbodemberg@gmail.com>
2026-01-08 22:08:31 -0300

    att mudanças essenciais

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+$(document).ready(function() {
+    // Abre e fecha o menu ao clicar no botão
+    $('.btn-mobile').on('click', function() {
+        $('.mobile-menu').toggleClass('active');
+        
+        // Troca o ícone de barras pelo de fechar (X)
+        const icon = $(this).find('i');
+        icon.toggleClass('fa-bars fa-xmark');
+    });
 
+    // Fecha o menu automaticamente ao clicar em qualquer link
+    $('.mobile-list a').on('click', function() {
+        $('.mobile-menu').removeClass('active');
+        $('.btn-mobile i').addClass('fa-bars').removeClass('fa-xmark');
+    });
+});
 
 //btn faq
 const faqItems = document.querySelectorAll('.faq-item');
