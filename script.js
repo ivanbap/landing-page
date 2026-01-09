9819456da45a025f298dc66fb86081bf15762a8b
ivanbap <ivanbodemberg@gmail.com>
2026-01-08 21:30:49 -0300

    att remove mobile menu

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-$(document).ready(function() {
-    $('.btn-mobile').on('click', function () {
-        $('.mobile-menu').toggleClass('active');
-        $('.btn-mobile').find('i').toggleClass('fa-x');
-});
-});
+
 
 //btn faq
 const faqItems = document.querySelectorAll('.faq-item');
